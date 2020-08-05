28484671e4b5cd259ae560e7f756306131432e57
sethi51 <69237467+sethi51@users.noreply.github.com>
2020-08-05 14:49:42 +0530

    Added Diff Module

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -2,4 +2,7 @@ class Calculator{
   public void add(int a,int b){
     System.out.println("Sum = "+(a+b));
   }
+  public void diff(int a,int b){
+    System.out.println("Difference = "+(a-b));
+  }
 }
