751a7f623b97182d60ad773846cf4a091c218545
sethi51 <69237467+sethi51@users.noreply.github.com>
2020-08-05 15:09:04 +0530

    Div Module Added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -5,4 +5,7 @@ class Calculator{
   public void diff(int a,int b){
     System.out.println("Difference = "+(a-b));
   }
+  public void div(int a,int b){
+    System.out.println("Quotient = "+(a/b));
+  }
 }
